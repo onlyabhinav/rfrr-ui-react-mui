268c4e36f43a528f2db5922142c1d00981f7bc57
abhinav <abhinav.2310@outlook.com>
2023-10-05 21:13:27 +0530

    dynamic paged grid

--- src/components/pages/CustomersManageList.tsx
+++ src/components/pages/CustomersManageList.tsx
@@ -1,58 +1,71 @@
-import React, { useEffect, useState } from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Tooltip from '@mui/material/Tooltip';
-import IconButton from '@mui/material/IconButton';
-import SearchIcon from '@mui/icons-material/Search';
-import RefreshIcon from '@mui/icons-material/Refresh';
-import DenseTable from '../controls/DenseTable';
-import DataGridDemo from '../controls/DataGridDemo';
-import axios from 'axios';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
-
+import React, { useEffect, useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Tooltip from "@mui/material/Tooltip";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
+import RefreshIcon from "@mui/icons-material/Refresh";
+import DenseTable from "../controls/DenseTable";
+import DataGridDemo from "../controls/DataGridDemo";
+import axios from "axios";
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import TablePaged from "../controls/TablePaged";
 
 export default function CustomersManageList() {
-
   const [customers, setCustomers] = useState([]);
 
   useEffect(() => {
     // Define the API endpoint URL
-    const apiUrl = 'http://localhost:8081/api/v1/customer/getall'; // Replace with your API endpoint
+    const apiUrl = "http://localhost:8081/api/v1/customer/getall"; // Replace with your API endpoint
 
-    console.info('Getting Data from API...');
-
+    console.info("Getting Data from API...");
 
     // Fetch data from the API
-    axios.get(apiUrl)
+    axios
+      .get(apiUrl)
       .then((response) => {
-        console.info('Data received from API...');
+        console.info("Data received from API...");
         setCustomers(response.data);
       })
       .catch((error) => {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       });
   }, []); // Empty dependency array to fetch data only once when the component mounts
 
-
+  const columnsDef = [
+    "id",
+    "customerid",
+    "surname",
+    "email",
+    "phone",
+    "creditscore",
+    "geography",
+    "gender",
+    "age",
+    "tenure",
+    "balance",
+    "numofproducts",
+    "hascrcard",
+    "isactivemember",
+    "estimatedsalary",
+  ];
 
   return (
-    <Paper sx={{ maxWidth: 4000, margin: 'auto', overflow: 'hidden' }}>
-      <AppBar
-        position="static"
-        color="default"
-        elevation={0}
-        sx={{ borderBottom: '1px solid rgba(0, 0, 0, 0.12)' }}
-      >
-        <Button variant="outlined" color="info" >MANAGE CUSTOMER LIST</Button>
+    <Paper sx={{ maxWidth: 4000, margin: "auto", overflow: "hidden" }}>
+      <AppBar position="static" color="default" elevation={0} sx={{ borderBottom: "1px solid rgba(0, 0, 0, 0.12)" }}>
+        <Button variant="outlined" color="info">
+          MANAGE CUSTOMER LIST
+        </Button>
       </AppBar>
-      
 
-      <TableContainer component={Paper}>
+      <TablePaged rows={customers} columns={columnsDef} key="customerid" />
+
+      {/* <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
         <TableHead>
           <TableRow>
@@ -91,12 +104,11 @@ export default function CustomersManageList() {
           ))}
         </TableBody>
       </Table>
-    </TableContainer>
+    </TableContainer> */}
 
       {/* <DenseTable rows={customers}/> */}
       {/* <DataGridDemo/> */}
       {/* <SignUp/> */}
-
     </Paper>
   );
 }
